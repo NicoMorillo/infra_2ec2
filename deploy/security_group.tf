4b1de51df307e5c0954937da7409876ce50a2e06
Nicolas M. Morillo Martinez <82237470+NicoMorillo@users.noreply.github.com>
2023-05-28 01:55:28 +0100

    Update security_group.tf

--- deploy/security_group.tf
+++ deploy/security_group.tf
@@ -1,6 +1,6 @@
 resource "aws_security_group" "ec2_sg" {
   name        = "ec2_sg"
-  description = "ec2 ecurity group"
+  description = "ec2 security group"
   vpc_id      = module.vpc.vpc_id
 
   tags = {
